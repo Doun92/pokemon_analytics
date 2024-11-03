8341c37ccafdc85b38cbf548584c513b60ed4906
Daniel Escoval <34299110+Doun92@users.noreply.github.com>
2024-11-03 16:16:19 +0100

    sorted inside the lsit to get the pokemons page

--- get_data.py
+++ get_data.py
@@ -15,9 +15,25 @@ def get_anchor_text(page):
             list_to_return.append(anchor.text)
     return list_to_return
 
+def get_pokemon_data(page):
+    # print(page)
+    html_code = requests.get(page).content
+    # print(html_code)
+    soup = BeautifulSoup(html_code, 'html.parser')
+    # print(soup)
+    title = soup.find('title')
+    print(title.text)
+
 #Get all pokedex
 différentes_generations = get_anchor_text(pokemons_par_generation)
-print(différentes_generations)
+# print(différentes_generations)
 
-
+for génération in différentes_generations:
+    génération = génération.replace(" ", "_")
+    # print(génération)
+    liste_pokemon = get_anchor_text(f"https://www.pokepedia.fr/Cat%C3%A9gorie:{génération}")
+    liste_pokemon = [ x for x in liste_pokemon if "Pokémon" not in x and "Ultra-Chimère" not in x and "Espèce convergente" not in x] #Remove from the list any entry with the characters inside.
+    # print(liste_pokemon)
+    for pokemon in liste_pokemon:
+        get_pokemon_data(f"https://www.pokepedia.fr/{pokemon}")
 # Get all the pokemons from the pokedex page
